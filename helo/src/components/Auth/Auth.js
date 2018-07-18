0153af63b7b89dfb82d527f17f72ca383f7278b7
Jacob Dayton <jacob@Jacobs-MacBook-Pro.local>
2018-07-17 18:26:47 -0600

    setup completed

---
+++ helo/src/components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Auth</h3>
+            </div>
+        )
+    }
+}
---
+++ helo/src/components/Form/Form.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Form</h3>
+            </div>
+        )
+    }
+}
